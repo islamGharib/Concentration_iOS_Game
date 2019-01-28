3c71833636202a26b2792996298ddba207c3ce3c
Islam Gharib <islamgharib@islams-macbook-pro.local>
2019-01-28 12:47:53 +0200

    Supporting Files

--- Concentration/Supporting Files/AppDelegate.swift
+++ Concentration/Supporting Files/AppDelegate.swift
@@ -40,7 +40,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func resetApp() {
+        UIApplication.shared.windows[0].rootViewController = UIStoryboard(
+            name: "Main",
+            bundle: nil
+            ).instantiateInitialViewController()
+    }
+    
 
 }
 
